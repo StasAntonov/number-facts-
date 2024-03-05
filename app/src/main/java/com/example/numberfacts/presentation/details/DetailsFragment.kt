7e4abee3742703d7d89cf0d4294097384e483749
StasAntonov <andropa07stas@gmail.com>
2024-03-05 14:27:47 +0200

    added UI and navigation

---
+++ app/src/main/java/com/example/numberfacts/presentation/details/DetailsFragment.kt
@@ -0,0 +1,6 @@
+package com.example.numberfacts.presentation.details
+
+import com.example.numberfacts.databinding.FragmentDetailsBinding
+import com.example.numberfacts.presentation.base.BaseFragment
+
+class DetailsFragment: BaseFragment<FragmentDetailsBinding>(FragmentDetailsBinding::inflate)
---
+++ app/src/main/java/com/example/numberfacts/presentation/first/FirsFragment.kt
@@ -0,0 +1,6 @@
+package com.example.numberfacts.presentation.first
+
+import com.example.numberfacts.databinding.FragmentFirstBinding
+import com.example.numberfacts.presentation.base.BaseFragment
+
+class FirsFragment: BaseFragment<FragmentFirstBinding>(FragmentFirstBinding::inflate)
